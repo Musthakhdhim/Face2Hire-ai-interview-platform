c293b7953a11f4223eccb51ce0de70ff4dda17e4
musthak <musthakhdhimnpb@gmail.com>
2026-06-18 20:17:37 +0530

    fix(email): resolve mail sending error

--- face2hire-backend/src/main/java/com/aiinterview/face2hire_backend/Face2hireBackendApplication.java
+++ face2hire-backend/src/main/java/com/aiinterview/face2hire_backend/Face2hireBackendApplication.java
@@ -2,10 +2,12 @@ package com.aiinterview.face2hire_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class Face2hireBackendApplication {
 
     public static void main(String[] args) {
